9d01a18802d04a0c184df126e038d887fac215fd
Paul Anderson <eggsarebad@gmail.com>
2014-03-31 20:33:33 -0400

    Hello World

---
+++ Hello World/app.js
@@ -0,0 +1,42 @@
+//First we'll include express to manage routing
+var express = require('express'),
+app = express();
+
+
+//The use method adds middleware to the request stack... here's the default console logger
+app.use(express.logger());
+
+
+//Define homepage route
+app.get('/', function(req, res){
+	//Send response back to client
+    res.send('Thank you for watching!!!');
+});
+
+
+//Define other route
+app.get('/test', function(req, res){
+	//Send response back to client
+	res.send('You typed test');
+});
+
+
+//Define 404 by matching all other routes
+app.get('/*', function(req, res){
+	//Send HTTP status codes 404
+	res.status(404);
+	//Send response
+	res.send('Page not found');
+});
+
+
+//Start listing on port 3000
+app.listen(3000);
+//Output to console
+console.log('Express server started on port %s', 3000);
+
+
+//Remaining steps
+//- Install express module to our project folder "npm install express"
+//- Install nodemon using "-g" to make it globally available. This module will auto restart our project
+//  if changes are made. "npm install -g nodemon"
