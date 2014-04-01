80cd91f385ac362c186e34475f898f34cdb602e7
Paul Anderson <eggsarebad@gmail.com>
2014-03-31 20:38:08 -0400

    Update

--- Hello World/app.js
+++ Hello World/app.js
@@ -10,7 +10,7 @@ app.use(express.logger());
 //Define homepage route
 app.get('/', function(req, res){
 	//Send response back to client
-    res.send('Thank you for watching!!!');
+    res.send('Hello World');
 });
 
 
@@ -37,6 +37,6 @@ console.log('Express server started on port %s', 3000);
 
 
 //Remaining steps
-//- Install express module to our project folder "npm install express"
+//- Install express module to the project folder "npm install express"
 //- Install nodemon using "-g" to make it globally available. This module will auto restart our project
 //  if changes are made. "npm install -g nodemon"
